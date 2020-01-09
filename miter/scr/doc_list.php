113cb37f05a132853ed711b801846bfc16235e3a
Vije Miller <58710973+vijemiller@users.noreply.github.com>
2020-01-08 23:54:34 -0500

    Add files via upload

--- miter/scr/doc_list.php
+++ miter/scr/doc_list.php
@@ -1,25 +1,44 @@
 <?
-	echo "<table class='static_header'>
-	<tr><td class='static_header_td'>";
-	echo "<span class='title'>Documents</span>";
-	echo "</td></tr></table>";
-	
-	echo "<table class='static_table'>
-	<tr><td class='static_td'>
-	<span class='tenon'>";
-	
-	$i = 1;
-	$file_list = glob("doc/*.*");
-	foreach($file_list as $file_name) {
-		$file_name = str_replace("doc/","",$file_name);
-		echo "" . $i++ . ". <a href='doc/" . $file_name . "' target='_blank'>" . $file_name . "</a>";
-		
-		// delete
-		echo " <a href='scr/delete_doc.php?d=" . $file_name . "' onclick='return confirm_delete()'><img src='but/delete.jpg' class='archive_delete'></a>";
-		
-		echo "<br />";
-	}
-	
-	echo "</span>
-	</td></tr></table>";
+	function formatbytes($bytes, $precision = 2) { 
+		$units = array('b', 'kb', 'mb', 'gb', 'tb'); 
+		$bytes = max($bytes, 0); 
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+		$pow = min($pow, count($units) - 1); 
+		$bytes /= (1 << (10 * $pow)); 
+		// $bytes /= pow(1024, $pow);
+		return round($bytes, $precision) . ' ' . $units[$pow]; 
+	} 
 ?>
+
+<table class='static_header'>
+	<tr>
+		<td class='static_header_td'>
+			<span class='title'>Documents</span>
+		</td>
+	</tr>
+</table>
+
+<table class='static_table'>
+	<tr>
+		<td class='static_td'>
+			
+			<table style='width:100%;padding:0;margin:0;border:0px solid #fff;'>
+				
+				<?
+					$i = 1;
+					$file_list = glob("doc/*.*");
+					foreach($file_list as $file_name) {
+						$file_name = str_replace("doc/","",$file_name);
+						$file_loc = "doc/" . $file_name;
+						$file_size = filesize($file_loc);
+						
+						echo "<tr><td align='left'><span class='tenon'>" . $i++ . ". <a href='doc/" . $file_name . "' target='_blank'>" . substr($file_name, 0, 30) . "</a></span>";
+						echo "</td><td align='right'><span class='name'>" . formatbytes($file_size) . " <a href='scr/delete_doc.php?d=" . $file_name . "' onclick='return confirm_delete()'>X</a></span>";
+						echo "</td></tr>";
+					}
+				?>
+				
+			</table>
+		</td>
+	</tr>
+</table>
--- miter/scr/image_t.php
+++ miter/scr/image_t.php
@@ -1,27 +1,43 @@
 <?
-	
-	echo "<table class='static_header'>
-	<tr><td class='static_header_td'>";
-	echo "<span class='title'>Tenon Images</span>";
-	echo "</td></tr></table>";
-	
-	echo "<table class='static_table'>
-	<tr><td class='static_td'>
-	<span class='tenon'>";
-	
-	$i = 1;
-	$file_list = glob("img/*.*");
-	foreach($file_list as $file_name) {
-		$file_name = str_replace("img/","",$file_name);
-		echo "" . $i++ . ". <a href='img/" . $file_name . "' target='_blank'>" . $file_name . "</a>";
-		
-		// delete
-		echo " <a href='scr/delete_image.php?d=" . $file_name . "' onclick='return confirm_delete()'><img src='but/delete.jpg' class='archive_delete'></a>";
-		
-		echo "<br />";
-	}
-	
-	echo "</span>
-	</td></tr></table>";
-	
+	function formatbytes($bytes, $precision = 2) { 
+		$units = array('b', 'kb', 'mb', 'gb', 'tb'); 
+		$bytes = max($bytes, 0); 
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+		$pow = min($pow, count($units) - 1); 
+		$bytes /= (1 << (10 * $pow)); 
+		// $bytes /= pow(1024, $pow);
+		return round($bytes, $precision) . ' ' . $units[$pow]; 
+	} 
 ?>
+
+<table class='static_header'>
+	<tr>
+		<td class='static_header_td'>
+			<span class='title'>Tenon Images</span>
+		</td>
+	</tr>
+</table>
+
+<table class='static_table'>
+	<tr>
+		<td class='static_td'>
+			
+			<table style='width:100%;padding:0;margin:0;border:0px solid #fff;'>
+				
+				<?
+					$i = 1;
+					$file_list = glob("img/*.*");
+					foreach($file_list as $file_name) {
+						$file_name = str_replace("img/","",$file_name);
+						$file_loc = "img/" . $file_name;
+						$file_size = filesize($file_loc);
+						
+						echo "<tr><td align='left'><span class='tenon'>" . $i++ . ". <a href='img/" . $file_name . "' target='_blank'>" . substr($file_name, 0, 30) . "</a></span>";
+						echo "</td><td align='right'><span class='name'>" . formatbytes($file_size) . " <a href='scr/delete_image.php?d=" . $file_name . "' onclick='return confirm_delete()'>X</a></span>";
+						echo "</td></tr>";
+					}
+				?>
+			</table>
+		</td>
+	</tr>
+</table>
