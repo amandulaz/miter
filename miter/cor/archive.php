c0fb65a11bf6469c6d88f02da0ffe53d58b96bfa
Vije Miller <58710973+vijemiller@users.noreply.github.com>
2020-03-14 17:59:45 -0400

    Add files via upload

--- miter/cor/archive.php
+++ miter/cor/archive.php
@@ -13,6 +13,13 @@
 			<table style='width:100%;padding:0;margin:0;border:0px solid #fff;'>
 				
 				<?	
+					
+					if(isMobile()) {
+						$char_count = 30;
+						} else {
+						$char_count = 45;
+					}
+					
 					$i = 1;
 					$file_list = glob("ten/*.txt");
 					foreach($file_list as $file_name) {
@@ -20,8 +27,7 @@
 						$file_name = str_replace(".txt","",$file_name);
 						$file_print = str_replace("_"," ",$file_name);
 						
-						echo "<tr>
-						<td align='left'><span class='tenon'>" . $i++ . ". <a href='index.php?t=" . $file_name . "' target='_blank'>" . substr($file_print, 0, 30) . "</a></span>";
+						echo "<tr><td align='left'><span class='tenon'>" . $i++ . ". <a href='index.php?t=" . $file_name . "' target='_blank'>" . substr($file_print, 0, $char_count) . "</a></span>";
 						if($login == true) {
 							echo "</td><td align='right'><span class='name'><a href='index.php?e=" . $file_name . "#edit'><img src='but/edit.jpg' class='button_side'></a>&nbsp;&nbsp;&nbsp;";
 							echo "<a href='scr/delete_image_m.php?d=" . $file_name . "' onclick='return confirm_delete()'><img src='but/delete.jpg' class='button_side'></a></span>";
