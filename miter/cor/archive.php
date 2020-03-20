9299ff82bfc9456cd78c463f16a3c1ee9f14ac7f
Vije Miller <58710973+vijemiller@users.noreply.github.com>
2020-03-20 03:02:47 -0400

    Add files via upload

--- miter/cor/archive.php
+++ miter/cor/archive.php
@@ -30,7 +30,7 @@
 						echo "<tr><td align='left'><span class='tenon'>" . $i++ . ". <a href='index.php?t=" . $file_name . "' target='_blank'>" . substr($file_print, 0, $char_count) . "</a></span>";
 						if($login == true) {
 							echo "</td><td align='right'><span class='name'><a href='index.php?e=" . $file_name . "#edit'><img src='but/edit.jpg' class='button_side'></a>&nbsp;&nbsp;&nbsp;";
-							echo "<a href='scr/delete_image_m.php?d=" . $file_name . "' onclick='return confirm_delete()'><img src='but/delete.jpg' class='button_side'></a></span>";
+							echo "<a href='scr/delete_static_archive.php?d=" . $file_name . "' onclick='return confirm_delete()'><img src='but/delete.jpg' class='button_side'></a></span>";
 						}
 						echo "</td>
 						</tr>";
