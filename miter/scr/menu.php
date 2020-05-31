118371b96c65d665560bd310035daea67fa2d909
Vije Miller <58710973+vijemiller@users.noreply.github.com>
2020-05-31 19:40:46 -0400

    Add files via upload

--- miter/scr/menu.php
+++ miter/scr/menu.php
@@ -7,13 +7,16 @@
 		<tr><td class="menu_td">
 			
 			<a href="?u=new" style="color:#111111;">New Tenon</a><br />
-			<a href="?u=upload" style="color:#111111;">Upload(s)</a><br />
+			<a href="?u=upload" style="color:#111111;">Upload</a><br />
 			<a href="?a=" style="color:#111111;">Tenon Archive</a><br />
 			<a href="?u=t_images" style="color:#111111;">Tenon Images</a><br />
 			<a href="?u=docs" style="color:#111111;">Tenon Files</a><br />
+			<hr>
 			<a href="?u=m_images" style="color:#111111;">Miter Images</a><br />
+			<hr>
 			<a href="?u=panels" style="color:#111111;">Panels</a><br />
 			<a href="?u=settings" style="color:#111111;">Settings</a><br />
+			<hr>
 			<a href="scr/logout.php" style="color:#111111;">Logout</a>
 			
 		</td>
